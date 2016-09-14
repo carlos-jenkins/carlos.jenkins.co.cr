83d3566112e346ca8bbfaf90c59f6dc57d69b3f8
Carlos Jenkins <carlos@jenkins.co.cr>
2016-09-14 00:55:41 -0600

    Fixed gallery JS and contact section.

--- themes/hugo-strata-theme/static/js/main.js
+++ themes/hugo-strata-theme/static/js/main.js
@@ -9,11 +9,10 @@
 	var settings = {
 
 		// Parallax background effect?
-			parallax: true,
+		parallax: true,
 
 		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 20
-
+		parallaxFactor: 20
 	};
 
 	skel.breakpoints({
@@ -99,7 +98,7 @@
 			// Lightbox gallery.
 				$window.on('load', function() {
 
-					$('#two').poptrox({
+					$('#portfolio').poptrox({
 						caption: function($a) { return $a.next('h3').text(); },
 						overlayColor: '#2c2c2c',
 						overlayOpacity: 0.85,
